de16599e3ad7978bcbf5e6da9ec68b106bfda2cd
Chayapol-c <chayapol.cha@ku.th>
2020-10-12 16:57:09 +0700

    add some Meeting test

--- appointment/tests.py
+++ appointment/tests.py
@@ -1,3 +1,40 @@
+"""Tests for Django project"""
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Meeting
+
+def create_meeting(subject, description, location, start_time, contact, end_time):
+    time = timezone.now() + datetime.timedelta(days=start_time)
+    time_end = timezone.now() + datetime.timedelta(days=end_time)
+    return Meeting.objects.create(subject = subject , description = description, location = location, start_time = time , contact = contact , end_time = time_end)
+
+class MeetingModelTest(TestCase):
+    """Tests for Django Model."""
+
+    def test_was_published_recently_with_future_meeting(self):
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_meeting = Meeting(start_time = time)
+        self.assertIs(future_meeting.was_published_recently(), False)
+
+
+# class IndexViewTest(TestCase):
+#     """Test for TewMa home page."""
+
+#     def test_no_meeting(self):
+#         response = self.client.get(reverse('appointment:index'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertContains(response, "No polls are available.")
+
+#     def test_past_meeting(self):
+#         meet = Meeting()
+#         response = self.client.get(reverse('appointment:index'))
+#         self.assertQuerysetEqual(
+#             response.context['latest_question_list'],
+#             ['<Meeting: Past Meeting.>']
+#         )
+
+    
+        
