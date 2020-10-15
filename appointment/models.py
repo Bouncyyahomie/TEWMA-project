33557c32f33b1f7cc3c50d30644c1f66ebedbb4e
Chayapol-c <chayapol.cha@ku.th>
2020-10-15 21:33:49 +0700

    comment user attribute

--- appointment/models.py
+++ appointment/models.py
@@ -1,13 +1,13 @@
 """Config for Django models."""
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 import datetime
 
 class Meeting(models.Model):
     """Django model Object for meeting."""
 
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    # user = models.ForeignKey(User , on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
