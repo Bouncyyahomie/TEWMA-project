0593ea1b32d55b35ec7d531d43bdcf118272d9b1
Chayapol-c <chayapol.cha@ku.th>
2020-10-15 21:35:31 +0700

    comment user attribute

--- appointment/models.py
+++ appointment/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Meeting(models.Model):
     """Django model Object for meeting."""
 
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    # user = models.ForeignKey(User , on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
