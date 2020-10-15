2a6accae057d90f427b23eb3cfe4c0b45e886b9b
Chayapol-c <chayapol.cha@ku.th>
2020-10-16 01:08:46 +0700

    formatting code

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,4 @@
+"""Views for Django appointment app."""
 from django.shortcuts import render
 from django.views import generic
 from .models import Meeting
@@ -5,23 +6,26 @@ from datetime import date, datetime
 from .utils import Calendar
 from django.utils.safestring import mark_safe
 
-# Create your views here.
 
 def get_date(req_day):
+    """Return specific date object if parameter is a date object, return today otherwise."""
     if req_day:
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
     return datetime.today()
 
+
 class IndexView(generic.ListView):
+    """Show a Calendar."""
+
     model = Meeting
     template_name = 'appointment/home_page.html'
 
     def get_context_data(self, **kwargs):
+        """Return context which is html_calendar."""
         context = super().get_context_data(**kwargs)
         this_day = get_date(self.request.GET.get('month', None))
         calendar = Calendar(this_day.year, this_day.month)
         html_calendar = calendar.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_calendar)
         return context
-
