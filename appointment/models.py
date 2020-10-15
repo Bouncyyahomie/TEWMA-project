2f53228f51e802a504d3bc31c5614329b35cb79a
Chayapol-c <chayapol.cha@ku.th>
2020-10-15 19:05:44 +0700

    edit Meeting model

--- appointment/models.py
+++ appointment/models.py
@@ -1,30 +1,24 @@
 """Config for Django models."""
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import datetime
 
 class Meeting(models.Model):
     """Django model Object for meeting."""
 
-    subject = models.CharField(max_length = 100)
-    description = models.CharField(max_length = 500)
-    location = models.CharField(max_length = 100)
-    start_time = models.DateTimeField('start time')
-    contact = models.CharField(max_length = 100)
-    end_time = models.DateTimeField('end time')
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    subject = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
     def __str__(self):
-        """Return string representation for Meeting."""
-        return f"{self.subject} : {self.location}"
+        return self.subject
 
-    def was_published_recently(self):
-        """Return true if question is published."""
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.start_time <= now
+    
 
-    def is_published(self):
-        """Return true if start date is before than current date."""
-        now = timezone.now()
-        return self.start_time <= now
+    
+
     
 
