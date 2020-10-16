9b9ae57af60b6451b3ab965c22f029bf87aeb06c
Chayapol-c <chayapol.cha@ku.th>
2020-10-16 18:09:44 +0700

    add get_html_url method and attributes

--- appointment/models.py
+++ appointment/models.py
@@ -1,18 +1,24 @@
 """Config for Django models."""
 from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Meeting(models.Model):
     """Django model Object for meeting."""
 
     # user = models.ForeignKey(User , on_delete=models.CASCADE)
-    subject = models.CharField(max_length=200)
+    subject = models.CharField(max_length=50)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    loaction = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
 
     def __str__(self):
         """Return string representative."""
         return self.subject
+
+    def get_html_url(self):
+        url = reverse("appointment:detail", args=self.id,)
+        return f'<a href="{url}"> {self.title} </a>'
