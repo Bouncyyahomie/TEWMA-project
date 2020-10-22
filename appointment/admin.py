122d253f359350d1468186d2f6c48486b3c282fa
chayapol <chayapol.cha@ku.th>
2020-10-22 14:45:34 +0700

    edit admin page

--- appointment/admin.py
+++ appointment/admin.py
@@ -2,5 +2,12 @@
 from django.contrib import admin
 from .models import Meeting
 
+class MeetingAdmin(admin.ModelAdmin):
 
-admin.site.register(Meeting)
+    fieldset  = [
+        (None,  {'fields': ['subject']}),
+        ('Infomation', {'fields': ['start_time']})
+    ]
+    list_display = ('subject','start_time','location','contact')
+
+admin.site.register(Meeting,MeetingAdmin)
