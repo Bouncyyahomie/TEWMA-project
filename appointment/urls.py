293665dd079b8c8c612c4716ff90f9ca4b98a90b
chayapol <chayapol.cha@ku.th>
2020-10-27 20:39:37 +0700

    edit url function

--- appointment/urls.py
+++ appointment/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="home_page"),
     path("meeting/<int:year>/<int:month>/<int:day>", views.meeting_list, name="meet_list"),
     path("<int:meeting_id>/detail", views.detail, name="detail"),
-    url(r'result/$', views.search, name="search"),
+    path('result/', views.search, name="search"),
 ]
