7302d1058a667e4a6761ec9086315c75b071fa8e
nuttapol-kor <nuttapol.kor@ku.th>
2020-11-01 05:09:19 +0700

    Create tests for user profile and format test

--- users/tests/test_login.py
+++ users/tests/test_login.py
@@ -2,8 +2,11 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 def create_user(username, email, password):
-    return User.objects.create_user(username=username,email=email,password=password)
+    """For create the user for test"""
+    return User.objects.create_user(username=username, email=email, password=password)
+
 
 class LogInTest(TestCase):
     """Class for test login system"""
@@ -11,17 +14,17 @@ class LogInTest(TestCase):
     def test_simple_login(self):
         """If can login, that user must be authenticated in website"""
         create_user("User1", "User1@gmail.com", "isp123456")
-        response = self.client.post(reverse('login'), {'username':'User1', 'password':'isp123456'}, follow=True)
+        response = self.client.post(reverse('login'), {'username': 'User1', 'password': 'isp123456'}, follow=True)
         self.assertTrue(response.context['user'].is_authenticated)
 
     def test_can_not_login_with_user_that_not_enter_password(self):
         """Can not login with enter the username but not enter the password"""
         create_user("User1", "User1@gmail.com", "isp123456")
-        response = response = self.client.post(reverse('login'), {'username':'User1'}, follow=True)
+        response = response = self.client.post(reverse('login'), {'username': 'User1'}, follow=True)
         self.assertFalse(response.context['user'].is_authenticated)
 
     def test_can_not_login_with_user_that_enter_wrong_password(self):
         """Can not login, if enter the wrong password"""
         create_user("User1", "User1@gmail.com", "isp123456")
-        response = self.client.post(reverse('login'), {'username':'User1', 'password':'isp555555'}, follow=True)
+        response = self.client.post(reverse('login'), {'username': 'User1', 'password': 'isp555555'}, follow=True)
         self.assertFalse(response.context['user'].is_authenticated)
