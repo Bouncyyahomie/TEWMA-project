85a3ed6c9072e433f682ec0f5445b9a0ebe1d7b2
chayapol <chayapol.cha@ku.th>
2020-11-30 01:46:04 +0700

    update requirement.txt

--- tewma/settings.py
+++ tewma/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
