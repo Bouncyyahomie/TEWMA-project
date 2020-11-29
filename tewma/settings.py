0dacc7028ae49002e2cdd1ef7dff14623c9465c7
chayapol <chayapol.cha@ku.th>
2020-11-30 01:59:20 +0700

    update requirement.txt

--- tewma/settings.py
+++ tewma/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
